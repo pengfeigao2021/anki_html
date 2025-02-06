058db2449e0d411b4e4c922a20958e2b7ad0606b
pengfeigao2021 <pengfeigao2021@163.com>
2025-02-06 00:30:07 -0800

    add daily schedule html
    Signed-off-by: pengfeigao2021 <pengfeigao2021@163.com>

--- run_upload.sh
+++ run_upload.sh
@@ -1,9 +1,11 @@
+# reset to tag and push changes
+base_tag="anki_v1.2"
 cd /Users/AlexG/Documents/GitHub/anki_html
 # remove unused commits
 git reflog expire --expire=now --all
 git gc --prune=now --aggressive
 # add files
-git reset --soft anki_v1.2
+git reset --soft $base_tag
 git add .
 d=`date +%Y%m%d\(%H:%M:%S\)`
 echo $d
